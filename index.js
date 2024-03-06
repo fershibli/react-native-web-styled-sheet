026c05c5d2b0d69cbc79c5a2c6c56956c679b2a1
FerShibli <fernandoluiz.1991@gmail.com>
2024-03-06 13:18:12 -0300

    Add web rendering support for React Native app

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 /**
  * @format
  */
+import {createRoot} from 'react-dom/client';
+import {Platform} from 'react-native';
 
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+if (Platform.OS === 'web') {
+  const root = createRoot(document.getElementById('root'));
+  // eslint-disable-next-line react/react-in-jsx-scope
+  root.render(<App />);
+}
